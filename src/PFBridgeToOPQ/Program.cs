9b336f2e71af4f607b2319cee615b05dd1df888e
littlegao233 <gaoxinhong2004@hotmail.com>
2020-12-26 17:25:21 +0800

    opq exe generate

---
+++ src/PFBridgeToOPQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Traceless.OPQSDK.Models;
+using Traceless.OPQSDK.Models.Event;
+using Traceless.OPQSDK.Models.Msg;
+using Traceless.Robot.Plugins;
+using Traceless.SocketIO;
+using Traceless.SocketIO.Messages;
+using Traceless.Utils.Ai.Tencent;
+
+namespace Traceless.Robot
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await OPQSDK.Plugin.OPQMain.Client();
+            while (true)
+            {
+                string temp = Console.ReadLine();
+            }
+        }
+    }
+}
--- src/PFBridgeToOPQ/Program.vb
+++
@@ -1,8 +0,0 @@
-Imports System
-Imports Traceless
-
-Module Program
-    Async Function Main() As Task
-        Await OPQSDK.Plugin.OPQMain.Client()
-    End Function
-End Module
