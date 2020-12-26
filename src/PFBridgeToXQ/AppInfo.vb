2e780292d66409f95e09a6f7baa34689de413c8e
littlegao233 <gaoxinhong2004@hotmail.com>
2020-12-26 15:53:37 +0800

    add OPQSDK

--- src/PFBridgeToXQ/AppInfo.vb
+++ src/PFBridgeToXQ/AppInfo.vb
@@ -1,6 +1,6 @@
 ﻿Imports XQ.Net.SDK.Attributes
 <Assembly: XQPlugin(
-    "PFBridge",
+    "PFBridge.XQ",
     "littlegao233",
     "1.0.0",
     "PFBridgeToXQ beta by gxh (github:https://github.com/littlegao233/PFBridge)"
