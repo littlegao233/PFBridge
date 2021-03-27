6508490bb12ae6d257b80b9fa0bcff23199ae90f
littlegao233 <gaoxinhong2004@hotmail.com>
2021-03-27 17:04:52 +0800

    Revert "Revert "sync""
    This reverts commit 37dec55a65ad0908754e9aa909283a3cfdfc5403.

--- src/PFBridgeForOneBot/PluginMain.cs
+++ src/PFBridgeForOneBot/PluginMain.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Sora.Server;
 using static PFBridgeCore.APIs.EventsMap.QQEventsMap;
 using PFBridgeCore;
-
+using Sora.OnebotModel;
 namespace PFBridgeForOneBot
 {
     class PluginMain
@@ -19,7 +18,7 @@ namespace PFBridgeForOneBot
             PFBridgeCore.Main.Init(new API(api));
             hasStarted = true;
         }
-        internal static void Init(SoraWSServer server)
+        internal static void Init(Sora.Interfaces.ISoraService server)
         {
             server.Event.OnFileUpload += (sender, eventArgs) =>
             {
@@ -29,11 +28,11 @@ namespace PFBridgeForOneBot
                     //e.SoraApi.GetGroupMemberInfo
                     PFBridgeCore.APIs.Events.QQ.OnGroupMessage.Invoke(new GroupMessageEventsArgs(e.SourceGroup.Id, e.Sender.Id, $"群文件：{ e.FileInfo.Name }[{ e.FileInfo.Size}]",
 () =>
-                          {
-                              var get = e.SourceGroup.GetGroupInfo().AsTask();
-                              get.Wait();
-                              return get.Result.groupInfo.GroupName;
-                          },
+{
+    var get = e.SourceGroup.GetGroupInfo().AsTask();
+    get.Wait();
+    return get.Result.groupInfo.GroupName;
+},
                           () =>
                           {
                               var info = e.SourceGroup.GetGroupMemberInfo(e.Sender.Id).Result.memberInfo;
@@ -56,31 +55,31 @@ namespace PFBridgeForOneBot
                 return new ValueTask();
             };
             server.Event.OnGroupMessage += (sender, eventArgs) =>
-          {
-              try
-              {
-                  Sora.EventArgs.SoraEvent.GroupMessageEventArgs e = eventArgs;
-                  //e.SoraApi.GetGroupMemberInfo
-                  PFBridgeCore.APIs.Events.QQ.OnGroupMessage.Invoke(new GroupMessageEventsArgs(e.SourceGroup.Id, e.Sender.Id, CodeEx.CQDeCode(e.Message.RawText),
-                        () =>
-                        {
-                            var get = e.SourceGroup.GetGroupInfo().AsTask();
-                            get.Wait();
-                            return get.Result.groupInfo.GroupName;
-                        },
-                        () => e.SenderInfo.Nick,
-                        () => string.IsNullOrEmpty(e.SenderInfo.Card) ? e.SenderInfo.Nick : e.SenderInfo.Card,
-                        () => (int)e.SenderInfo.Role + 1,
-                        //     成员        Member = 0,
-                        //     管理员        Admin = 1,
-                        //     群主        Owner = 2
-                        s => e.Reply(s),
-                        () => CodeEx.ParseMessage(e.Message.RawText, e.SourceGroup.Id)
-                    ));
-              }
-              catch (Exception ex) { PFBridgeCore.APIs.API.LogErr(ex); }
-              return new ValueTask();
-          };
+            {
+                try
+                {
+                    Sora.EventArgs.SoraEvent.GroupMessageEventArgs e = eventArgs;
+                    //e.SoraApi.GetGroupMemberInfo
+                    PFBridgeCore.APIs.Events.QQ.OnGroupMessage.Invoke(new GroupMessageEventsArgs(e.SourceGroup.Id, e.Sender.Id, CodeEx.CQDeCode(e.Message.RawText),
+                          () =>
+                          {
+                              var get = e.SourceGroup.GetGroupInfo().AsTask();
+                              get.Wait();
+                              return get.Result.groupInfo.GroupName;
+                          },
+                          () => e.SenderInfo.Nick,
+                          () => string.IsNullOrEmpty(e.SenderInfo.Card) ? e.SenderInfo.Nick : e.SenderInfo.Card,
+                          () => (int)e.SenderInfo.Role + 1,
+                          //     成员        Member = 0,
+                          //     管理员        Admin = 1,
+                          //     群主        Owner = 2
+                          s => e.Reply(s),
+                          () => CodeEx.ParseMessage(e.Message.RawText, e.SourceGroup.Id)
+                      ));
+                }
+                catch (Exception ex) { PFBridgeCore.APIs.API.LogErr(ex); }
+                return new ValueTask();
+            };
             server.Event.OnClientConnect += (sender, eventArgs) =>
             {
                 AppStart(eventArgs.SoraApi);
